d40649d22cdfd6e1ecfada1cb7060e162a37647a
mark953fps <markville953@gmail.com>
2021-09-10 17:01:42 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  public searchForm: FormGroup;
+  public items: FormArray = new FormArray([]);
+  public dataFromApi: any = {
+    fname: 'Mark',
+    lname: 'Villegas',
+    items: [
+      {
+        id: 1,
+        itemName: 'item 1',
+        itemDescription: 'dummy text',
+        itemPrice: 100
+      },
+      {
+        id: 2,
+        itemName: 'item 2',
+        itemDescription: 'dummy text',
+        itemPrice: 200
+      },
+      {
+        id: 3,
+        itemName: 'item 3',
+        itemDescription: 'dummy text',
+        itemPrice: 300
+      }
+    ]
+  };
+
+  constructor(private fb: FormBuilder) {}
+
+  public ngOnInit(): void {
+    this.searchForm = this.fb.group({
+      fname: ['', Validators.required],
+      lname: ['', Validators.required],
+      items: this.fb.array([])
+    });
+
+    this.loadData();
+  }
+
+  public createForm(data: any): FormGroup {
+    return this.fb.group({
+      itemName: [data.itemName, Validators.required],
+      itemDescription: [data.itemDescription, Validators.required],
+      itemPrice: [data.itemPrice, Validators.required]
+    });
+  }
+
+  public loadData(): void {
+    if (this.dataFromApi) {
+      this.searchForm.controls.fname.setValue(this.dataFromApi.fname);
+      this.searchForm.controls.lname.setValue(this.dataFromApi.fname);
+
+      if (this.dataFromApi.items.length) {
+        this.dataFromApi.items.forEach(data => this.addItem(data));
+      }
+    }
+  }
+
+  public addItem(data): void {
+    this.items = this.searchForm.get('items') as FormArray;
+    this.items.push(this.createForm(data));
+  }
+
+  public removeItem(i): void {
+    this.items.removeAt(i);
+  }
+
+  public submit(): void {
+    console.log(this.searchForm.value, 'submit data');
+  }
 }
